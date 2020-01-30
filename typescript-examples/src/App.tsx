e191095624cef374684664c4557114ad65fc8ce6
Charlie McVicker <charlie@mcvickernet.com>
2020-01-30 17:57:28 -0500

    Updated typescript example

--- typescript-examples/src/App.tsx
+++ typescript-examples/src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ExampleComponent from 'react-mathjax';
+import { MathComponent } from 'react-mathjax';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ExampleComponent text="charlie's test" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props: {}) {
+    super(props);
+  }
+  render(){
+    const example = String.raw`\int_{-\infty}^{\infty}e^{-x^2}\ dx`;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <MathComponent tex={example} />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;

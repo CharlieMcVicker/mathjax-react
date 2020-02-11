54ab91d0bc9ce88daa55b9eff81f76c557438bb4
Charlie McVicker <charlie@mcvickernet.com>
2020-02-11 16:00:20 -0500

    Added /docs

---
+++ docs/build/precache-manifest.c27945c27f52bd217c91a6ed379deacb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47704d0136b9f06b680cf79253159bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "189726b9c41518419a6d",
+    "url": "/static/css/main.8e7fd8c3.chunk.css"
+  },
+  {
+    "revision": "b636ea422feef78bf5a7",
+    "url": "/static/js/2.7a9b2e8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7a9b2e8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "189726b9c41518419a6d",
+    "url": "/static/js/main.c445b40d.chunk.js"
+  },
+  {
+    "revision": "a6279281f3630f108fbb",
+    "url": "/static/js/runtime-main.f0bf48f8.js"
+  }
+]);
